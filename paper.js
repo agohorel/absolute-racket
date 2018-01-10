eff13ac6ccd9320640b55b222324805e51e0533e
agohorel <alex.oddlogic@gmail.com>
2018-01-09 20:41:43 -0500

    shapes generate from object method
    basic skeleton of how i'm thinking it will eventually work - object
    stores a bunch of methods for different shapes/colors/sounds which are
    triggered by keyboard input

--- paper.js
+++ paper.js
@@ -1,6 +1,21 @@
+var keys = {
+	a: {
+		shape: function(x, y, size){
+			return Path.Circle(x, y, size);
+		},
+		color: "#FFFFFF"
+	},
+	b: {
+		shape: function(x, y, size){
+			return Path.Circle(x, y, size);
+		},
+		color: "#FF0000"
+	}
+}
+
 function onKeyDown(event){
 	var x = Math.random() * view.size.width;
 	var y = Math.random() * view.size.height;
-	var newCircle = new Path.Circle(x, y, 100);
-	newCircle.fillColor = "red";
+	var newShape = keys[event.key].shape(x, y, 100);
+	newShape.fillColor = keys[event.key].color;
 }
