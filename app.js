60da0ae9561e9af3b576bd89d174f4a0e869b72e
agohorel <alex.oddlogic@gmail.com>
2018-01-10 18:18:59 -0500

    better filter controls & remove reverb (for now)
    bp filter now defaults to a cutoff of 0, mouseDragged controls cutoff,
    mouseReleased resets cutoff to 0.
    
    reverb was problematic, will need to rethink how to implement that.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var shapeSize = 300,
 	shapes = [],
-	reverb, 
-	bpFilter;
+	hpFilter;
 
-reverb = new p5.Reverb();
-bpFilter = new p5.HighPass();
+/////////////////// p5.js stuff ///////////////////
+
+hpFilter = new p5.HighPass();
 
 function preload(){
 	var kick = loadSound("./audio/kick.wav"),
@@ -15,19 +15,51 @@ function preload(){
 
 function setup(){
 	soundFormats("wav", "ogg");
-	// set initial reverb params
-	reverb.process(sounds[1], 0, 0);
 	// mute dry kick signal
 	sounds[0].disconnect();
 	// route kick signal to filter
-	sounds[0].connect(bpFilter);
+	sounds[0].connect(hpFilter);
+	// default bp freq
+	freq = 0;
+	hpFilter.freq(freq);
 }
 
-function draw(){
-	var freq = map(mouseX, 0, windowWidth, 20, 300);
-	bpFilter.freq(freq);
+function mouseDragged(){
+	var freq = map(mouseX, 0, windowWidth, 20, 500);
+	hpFilter.freq(freq);
 	console.log(freq);
 }
+
+function mouseReleased(){
+	freq = 0;
+	hpFilter.freq(freq);
+}
+
+/////////////////// paper.js stuff ///////////////////
+
+function onKeyDown(event){
+	// check if pressed key exists in keys object
+	if (keys[event.key]){
+		var newShape = keys[event.key].shape();
+		newShape.fillColor = keys[event.key].color;
+		shapes.push(newShape);
+	} else {
+		console.log("You pressed the " + event.key + " which does not exist in the keys object.");
+	}	
+}
+
+function onFrame(event){
+	// decrement loop to avoid splice() fuckery
+	for (var i = shapes.length - 1; i >= 0; i--){
+		shapes[i].fillColor.hue += 2;
+		shapes[i].scale(.85);
+		if (shapes[i].area < 1){
+			shapes[i].remove();
+			shapes.splice(i, 1);
+		}
+	}	 
+}
+
 
 var keys = {
 	a: {
@@ -56,39 +88,8 @@ var keys = {
 	}
 }
 
-function onKeyDown(event){
-	// check if pressed key exists in keys object
-	if (keys[event.key]){
-		var newShape = keys[event.key].shape();
-		newShape.fillColor = keys[event.key].color;
-		shapes.push(newShape);
-	} else {
-		console.log("You pressed the " + event.key + " which does not exist in the keys object.");
-	}	
-}
-
-function onFrame(event){
-	// decrement loop to avoid splice() fuckery
-	for (var i = shapes.length - 1; i >= 0; i--){
-		shapes[i].fillColor.hue += 2;
-		shapes[i].scale(.85);
-		if (shapes[i].area < 1){
-			shapes[i].remove();
-			shapes.splice(i, 1);
-		}
-	}	 
-}
-
 function randomCoords(){
 	var x = Math.random() * view.size.width;
 	var y = Math.random() * view.size.height;
 	return coords = new Point(x, y);
-}
-
-// change reverb params - problematic (causes lag)
-function mousePressed(){
-	console.log("the mouse has been moved to " + mouseX, mouseY);
-	var duration = map(mouseX, 0, 1300, 0, 8);
-	var decay = map(mouseY, -1000, 0, 0, 90);
-	reverb.set(duration, decay);
 }
