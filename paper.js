3ae2dd0739bccd64f689d71440b256fd6257d240
agohorel <alex.oddlogic@gmail.com>
2018-01-09 20:46:19 -0500

    shrinking shapes

--- paper.js
+++ paper.js
@@ -13,9 +13,19 @@ var keys = {
 	}
 }
 
+var shapes = [];
+
 function onKeyDown(event){
 	var x = Math.random() * view.size.width;
 	var y = Math.random() * view.size.height;
 	var newShape = keys[event.key].shape(x, y, 100);
 	newShape.fillColor = keys[event.key].color;
+	shapes.push(newShape);
+}
+
+function onFrame(event){
+	for (var i = 0; i < shapes.length; i++){
+		shapes[i].fillColor.hue += 1;
+		shapes[i].scale(.8);
+	}
 }
