49c7eab6c24ea0cea946b18800e09021010331d5
agohorel <alex.oddlogic@gmail.com>
2018-01-27 15:20:06 -0500

    add spacing btwn freq bands

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ function draw(){
 	for (var i = 0; i < spectrum.length; i++){
 		var x = map(i, 0, spectrum.length, 0, width);
 		var h = -height + map(spectrum[i], 0, 255, height, 0);
-		rect(x, height, width/spectrum.length, h);
+		rect(x, height, width/spectrum.length * .5, h);
 	}
 }
 
